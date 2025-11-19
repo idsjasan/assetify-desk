c8fa2feea07d55cf35de174cc6df955c2ef01d35
Seungpyo Suh <me@sspzoa.io>
2025-11-20 08:46:55 +0900

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-dvh justify-center items-center">
+      <span className="text-display font-bold">
+        Assetify <span className="text-core-accent">Desk.</span>
+      </span>
+    </div>
+  );
+}
