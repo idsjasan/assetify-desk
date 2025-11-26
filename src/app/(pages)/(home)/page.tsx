8c0ca9d5bda8f5c2b960f4c585b2db264c6db74a
Seungpyo Suh <me@sspzoa.io>
2025-11-26 10:11:30 +0900

    feat: home page

---
+++ src/app/(pages)/(home)/page.tsx
@@ -0,0 +1,36 @@
+import MenuButton from "@/app/(pages)/(home)/(components)/menuButton";
+
+export default function Home() {
+  return (
+    <div className="flex w-full flex-col items-center justify-start gap-spacing-700 px-spacing-400 py-spacing-700 md:h-dvh md:justify-center">
+      <span className="font-semibold text-display">
+        Assetify{" "}
+        <span className="bg-gradient-to-r from-core-accent to-solid-red bg-clip-text text-title text-transparent md:text-display">
+          Desk.
+        </span>
+      </span>
+      <div className="grid w-full max-w-3xl grid-cols-1 gap-spacing-400 md:grid-cols-2">
+        <MenuButton
+          href="/inquiry"
+          title="문의하기"
+          description="자산 관련 문의 사항을 해결할 수 있어요."
+        />
+        <MenuButton
+          href="/assets"
+          title="수리 요청하기"
+          description="하드웨어 고장이 난 경우 수리를 요청할 수 있어요."
+        />
+        <MenuButton
+          href="/license"
+          title="라이센스 찾기"
+          description="기존에 지급받은 라이센스를 다시 확인할 수 있어요."
+        />
+        <MenuButton
+          href="/assets"
+          title="자산 관리"
+          description="바코드를 통해 자산 정보를 확인할 수 있어요."
+        />
+      </div>
+    </div>
+  );
+}
